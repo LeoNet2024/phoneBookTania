f1936935c5646dda49350ae2f14f2cd212e29dff
Leon <162373739+LeoNet2024@users.noreply.github.com>
2025-04-23 19:49:20 +0300

    verson 4.23
    adding new components, adding login page, and imporve css UI

--- links/src/pages/Login.jsx
+++ links/src/pages/Login.jsx
@@ -7,87 +7,97 @@ import { useState } from "react";
 import { users } from "../data/users";
 
 import { checkUserName, checkPassword } from "../functions/validation";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [showPass, setShowPass] = useState("password");
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [vertify, setVertify] = useState(false);
+  const navigate = useNavigate();
+  let vertify;
+  const [userDetails, setUserDetails] = useState({
+    userName: "",
+    password: "",
+    conPassword: "",
+  });
 
-  function handleChangeUn(e) {
-    setUserName(e.target.value);
-  }
+  function handleSubmit(e) {
+    console.log("prevent");
+    e.preventDefault();
+    // cheking if the user Exists and also if the password the same
+    if (userDetails.password !== userDetails.conPassword) {
+      alert("passwords must be the same");
+      return;
+    }
 
-  function handleChangeUp(e) {
-    setPassword(e.target.value);
-  }
-
-  function handleSubmit() {
-    let result =
-      checkUserName(userName) &&
-      checkPassword(password) &&
-      users.find((el) => el.password === password && el.userName === userName);
-
-    if (result) setVertify(true);
-  }
-
-  if (vertify) return <Navigate to={"/home"} />;
-  else
-    return (
-      <div className={classes.page}>
-        <main>
-          <h2>Log in</h2>
-          <form action="" autoComplete="off">
-            <div className={classesLogIn.container}>
-              <div>
-                <label>User Name</label>
-
-                <input
-                  type="text"
-                  placeholder="User name"
-                  minLength={6}
-                  pattern="[A-za-z1-9]{6,12}"
-                  required
-                  onChange={(e) => handleChangeUn(e)}
-                />
-                <p className={classesLogIn.passUserDetails}>
-                  {" "}
-                  User name must contains letters and numbers only
-                </p>
-              </div>
-              <div>
-                <label>Password</label>
-                <input
-                  type={showPass}
-                  placeholder="Enter password"
-                  required
-                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                  title="invalid password"
-                  onChange={(e) => handleChangeUp(e)}
-                />
-                <FaEyeSlash
-                  onClick={() =>
-                    setShowPass((prev) =>
-                      prev === "text" ? "password" : "text"
-                    )
-                  }
-                />
-                <p className={classesLogIn.passUserDetails}>
-                  password must contain at least Captial letter and Number
-                </p>
-              </div>
-
-              <div>
-                <button type="submit" onClick={() => handleSubmit()}>
-                  Submit
-                </button>
-                <p>user name: leonleon</p>
-                <p>Password: leonleon</p>
-              </div>
-            </div>
-          </form>
-        </main>
-      </div>
+    vertify = users.find(
+      (el) =>
+        el.userName === userDetails.userName &&
+        el.password === userDetails.password
     );
+
+    if (vertify) navigate("/home");
+    else alert("password or user name is invalid");
+  }
+
+  function handleChange(e) {
+    setUserDetails((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  return (
+    <div className={classes.page}>
+      <main>
+        <h2>Log in</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <div className={classesLogIn.container}>
+            <div>
+              <label>User Name</label>
+              <input
+                type="text"
+                name="userName"
+                minLength={6}
+                pattern="[A-za-z1-9]{6,12}"
+                required
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                type={showPass}
+                required
+                name="password"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                onChange={(e) => handleChange(e)}
+              />
+              <FaEyeSlash
+                onClick={() =>
+                  setShowPass((prev) => (prev === "text" ? "password" : "text"))
+                }
+              />
+            </div>
+
+            <div>
+              <label>confirm Password</label>
+              <input
+                type={showPass}
+                required
+                name="conPassword"
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+
+            <div>
+              <button type="submit" onClick={() => handleSubmit()}>
+                Submit
+              </button>
+              <p>user name: leonleon</p>
+              <p>Password: Leon1996</p>
+            </div>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
